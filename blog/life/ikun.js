f5a621b66c4d8da0aaaa36e94fc08dc6775d6cb2
laine001 <40457081+laine001@users.noreply.github.com>
2024-11-07 09:56:14 +0800

    😂feat: create ikun.js

---
+++ blog/life/ikun.js
@@ -0,0 +1,14 @@
+const ikunSum = [
+  {
+    title: "《家有ikun》-童年封神金曲！百万ikun大合唱！全网首发！！",
+    author: "@bilibili 上海滩许Van强",
+    link: "https://www.bilibili.com/video/BV1Bu411a7Ht/",
+    playCount: "391.5"
+  },
+  {
+    title: "IKUN小黑子编年史！",
+    author: "趣料君",
+    link: "https://www.bilibili.com/video/BV1a24y1z7Ww/",
+    playCount: "132.0"
+  }
+]
